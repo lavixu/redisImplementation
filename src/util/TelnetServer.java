ddb1c36d79d098088cd522c098a3778f92a1478f
lavixu <lavanya.teja@gmail.com>
2013-11-28 01:15:40 +0530

    handled exit case for telnet

--- src/util/TelnetServer.java
+++ src/util/TelnetServer.java
@@ -1,56 +1,63 @@
 package util;
 
+import java.io.BufferedReader;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.EOFException;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.logging.Logger;
 
-
 import serialization.PersistData;
 
 public class TelnetServer {
-	private final Logger LOGGER = Logger.getLogger(TelnetServer.class.getName());
+	private final Logger LOGGER = Logger
+			.getLogger(TelnetServer.class.getName());
 	private final int PORT = 15000;
 	private static CommandParser commandParser = new CommandParser();
 	private static ServerSocket serverSocket = null;
 
-	
 	public TelnetServer() throws IOException {
 		serverSocket = new ServerSocket(PORT);
-		serverSocket.setSoTimeout(15*60*1000); //timeout at 15 minutes
+		serverSocket.setSoTimeout(30 * 60 * 1000); // timeout at 15 minutes
 	}
 
 	public void run() throws IOException {
-		Socket server = null;
 		while (true) {
+			Socket server = null;
 			try {
-
 				server = serverSocket.accept();
-				DataInputStream in = new DataInputStream(server.getInputStream());
-				String command = in.readUTF();
-				while (command != null && !command.equalsIgnoreCase("exit")) {
-					String result = commandParser.parseCommand(command);
-					DataOutputStream out = new DataOutputStream(server.getOutputStream());
-					out.writeUTF(result);
-					command = in.readUTF();
-				}
+				final BufferedReader reader = new BufferedReader(
+						new InputStreamReader(server.getInputStream()));
+				final PrintWriter out = new PrintWriter(
+						server.getOutputStream(), true);
+				boolean cancel = false;
+				while (!cancel) {
 
-			} 
-			catch(EOFException ignore)
-			{
+					String command = reader.readLine();
+					if (command == null || command.equalsIgnoreCase("exit")) {
+						cancel = true;
+						break;
+					}
+
+					String result = commandParser.parseCommand(command);
+					LOGGER.info(command);
+					out.println(result);
+					command = reader.readLine();
+				}
+			} catch (EOFException ignore) {
 				LOGGER.info("Client terminated.");
-			}
-			catch (IOException e) {
+			} catch (IOException e) {
 				LOGGER.severe(e.getMessage());
 			}
-			
+
 			finally {
 				PersistData.save();
+				server.close();
 			}
 		}
 	}
-
 }
